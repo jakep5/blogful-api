05ea537ccf5d72d33d30e363bf683ead0b3d7c04
jakep5 <jakepagel1@yahoo.com>
2019-11-11 14:04:23 -0600

    add second test instance for blogful

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -16,4 +16,49 @@ describe('Articles Endpoints', function() {
 
     before('clean the table', () => db('blogful_articles').truncate()) //clears the table before tests
 
+    context('Given there are articles in the database', () => {
+        const testArticles = [
+            {
+                id: 1,
+                date_published: '2029-01-22T16:28:32.615Z',
+                title: 'First test post!',
+                style: 'How-to',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
+            },
+            {
+                id: 2,
+                date_published: '2100-05-22T16:28:32.615Z',
+                title: 'Second test post!',
+                style: 'News',
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
+            },
+            {
+                id: 3,
+                date_published: '1919-12-22T16:28:32.615Z',
+                title: 'Third test post!',
+                style: 'Listicle',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
+            },
+            {
+                id: 4,
+                date_published: '1919-12-22T16:28:32.615Z',
+                title: 'Fourth test post!',
+                style: 'Story',
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam. Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam?'
+            },
+        ];
+
+        beforeEach('insert articles', () => { //inserts test articles before each test occurs - insures that all tests will be compared to these 4 articles
+            return db
+                .into('blogful_articles')
+                .insert(testArticles)
+        })
+        
+        it('GET /articles responds with 200 and all of the articles are returned', () => {
+            return supertest(app)
+                .get('/articles')
+                .expect(200)
+        })
+    })
+
 })
