460a722a5ada3a181ccf3701db9556ad65f76bc2
jakep5 <jakepagel1@yahoo.com>
2019-11-11 13:42:51 -0600

    begin server code

--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
+const knex = require('knex')
 const app = require('./app')
+const { PORT, DB_URL } = require('./config')
+
+const db = knex({
+    client: 'pg',
+    connection: DB_URL,
+})
+
 
 const { PORT } = require('./config')
 
