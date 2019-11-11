a82d49da78a10867429a689f802564663546b792
jakep5 <jakepagel1@yahoo.com>
2019-11-11 17:15:42 -0600

    finalize post request code, create articles-router

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { NODE_ENV } = require('./config')
 const ArticlesService = require('./articles-service')
 
 const app = express()
+const jsonParser = express.json()
 
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
@@ -55,8 +56,20 @@ app.get('/articles/:article_id', (req, res, next) => {
         .catch(next)
 })
 
-app.post('/articles', (req, res, next) => {
-    res.status(201).send('stuff')
+app.post('/articles', jsonParser, (req, res, next) => {
+    const { title, content, style } = req.body
+    const newArticle = { title, content, style }
+    ArticlesService.insertArticle(
+        req.app.get('db'),
+        newArticle
+    )
+        .then(article => {
+            res
+                .status(201)
+                .location(`/articles/${article.id}`)
+                .json(article)
+        })
+        .catch(next)
 })
 
 app.use(function errorHandler(error, req, res, next) {
