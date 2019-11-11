315972e06e5f2741f94e1aec5dba420a2aceea55
jakep5 <jakepagel1@yahoo.com>
2019-11-11 13:59:37 -0600

    add test file for blogful

---
+++ test/articles-endpoints.spec.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai')
+const knex = require('knex')
+const app = require('../src/app')
+
+describe('Articles Endpoints', function() {
+    let db
+
+    before('make knex instance', () => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DB_URL,
+        })
+    })
+
+    after('disconnect from db', () => db.destroy())
+
+    before('clean the table', () => db('blogful_articles').truncate()) //clears the table before tests
+
+})
